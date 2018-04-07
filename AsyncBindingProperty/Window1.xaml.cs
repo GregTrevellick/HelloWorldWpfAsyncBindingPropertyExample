854dc22725dc6e15b479be20b78a9674a9c76510
Greg Trevellick <gtrevellick@googlemail.com>
2018-04-07 21:53:44 +0100

    remove unused using directives

--- AsyncBindingProperty/Window1.xaml.cs
+++ AsyncBindingProperty/Window1.xaml.cs
@@ -1,30 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace AsyncBindingProperty
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
         SlowSource source = new SlowSource();
+
         public Window1()
         {
             InitializeComponent();
 
             this.DataContext = source;
+
+            //Upon start up do the slow thing without locking up the UI...
+            source.FetchNewData();
         }
 
         private void getNewDataButton_Click(object sender, RoutedEventArgs e)
